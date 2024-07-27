e53b61589d20305615050c6c4e6423ec0fefa3e4
Egba Frank <Egbafrank72@gmail.com>
2024-07-27 23:45:50 +0000

    Added some mandatory tasks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a Hello"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Return C text value"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+if __name__ == "__main__":
+    app.run()
